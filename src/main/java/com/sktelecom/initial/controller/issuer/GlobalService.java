295fcb5495125acf30c288e286e18b2e4654010a
khujin1 <khujin@gmail.com>
2021-06-10 14:25:27 +0900

    person_name 변수를 VC 발행

--- src/main/java/com/sktelecom/initial/controller/issuer/GlobalService.java
+++ src/main/java/com/sktelecom/initial/controller/issuer/GlobalService.java
@@ -519,9 +519,11 @@ public class GlobalService {
         String connectionId = JsonPath.read(body, "$.connectionId");
         String selectedItemId = JsonPath.read(body, "$.selectedItemId");
 
+        LinkedHashMap<String, String> attrs = getPresentationResult(body);
+
         // 3-1-1. 추가 정보 기반으로 증명서 발행
         log.info("sendCredentialOffer with connectionId:" + connectionId + ", selectedItemId:" + selectedItemId);
-        sendCredentialOffer(connectionId, null, selectedItemId);
+        sendCredentialOffer(connectionId, attrs, selectedItemId);
     }
 
     public void revokeCredential(String credExId) {
