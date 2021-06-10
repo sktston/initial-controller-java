18ecbb1710557587b0f66685a2e16e4d3cd56ee0
JJ <khujin@gmail.com>
2021-06-10 22:13:54 +0900

    날짜 random 변수 추가

--- src/main/java/com/sktelecom/initial/controller/issuer/GlobalService.java
+++ src/main/java/com/sktelecom/initial/controller/issuer/GlobalService.java
@@ -463,11 +463,11 @@ public class GlobalService {
         value.put("korean_name", attrs.get("person_name"));
         //log.info(attrs.get("person_name"));
         value.put("english_name", "Kim Initial");
-        value.put("registration_number", "123456789");
+        value.put("registration_number", attrs.get("mobile_num"));
         value.put("exp_date", getRandomInt(2021, 2022) + "0228");
         value.put("date_of_birth", "20000228");
         value.put("date_of_test", "20220228");
-        value.put("score_of_listening", "445");
+        value.put("score_of_listening", getRandomInt(100, 444) + "");
         value.put("score_of_reading", "");
         value.put("score_of_total", "990");
 
